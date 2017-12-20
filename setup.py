a8cc1e84052034cb7a98e6e2506dae1cdb3715d0
James T <jtara@tuta.io>
2017-12-19 16:48:04 -0800

    updates before pypi release

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from distutils.core import setup
 import re
 
 
+version = '0.1.1'
+
+
 def get_install_requirements():
     requirements = []
     with open('requirements.txt', 'r') as f:
@@ -10,15 +13,22 @@ def get_install_requirements():
     print(requirements)
     return requirements
 
+
 if __name__ == "__main__":
     pass
 
+
 setup(name='get_media_files',
-      version='v0.1',
+      packages=['get_media_files'],
+      version=version,
       description='Wrapper for pymediainfo that retrieves information such as'
                   'media type, duration, resolution, etc. from media files',
       author='James T',
       author_email='jtara@tuta.io',
-      url='https://github.com/jtara1/GetMediaInfo',
+      url='https://github.com/jtara1/get_media_info',
+      download_url='https://github.com/jtara1/get_media_files/archive/{version}.tar.gz'
+      .format(version=version),
+      keywords=['pymediainfo', 'wrapper', 'get media files', 'info', 'duration'],
       install_requires=get_install_requirements(),
+      classifiers=[]
       )

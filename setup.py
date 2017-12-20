10d40c5b171c9afa8b29194f7592d0761c282dda
James T <jtara@tuta.io>
2017-12-20 15:09:14 -0800

    update setup.py with more info

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ import re
 
 
 version = '0.1.2'
-github_url = 'https://github.com/jtara1/get_media_files'
+module_name = 'get_media_files'
+github_url = 'https://github.com/jtara1/{}'.format(module_name)
+with open('README.rst') as f:
+    readme = f.read()
 
 
 def get_install_requirements():
@@ -19,17 +22,20 @@ if __name__ == "__main__":
     pass
 
 
-setup(name='get_media_files',
-      packages=['get_media_files'],
+setup(name=module_name,
+      packages=[module_name],
       version=version,
       description='Wrapper for pymediainfo that retrieves information such as'
                   'media type, duration, resolution, etc. from media files',
+      long_description=readme,
       author='James T',
       author_email='jtara@tuta.io',
       url=github_url,
       download_url='{github_url}/archive/{version}.tar.gz'
       .format(github_url=github_url, version=version),
-      keywords=['pymediainfo', 'wrapper', 'get media files', 'info', 'duration'],
+      keywords=['pymediainfo', 'wrapper', 'get media files', 'info'],
       install_requires=get_install_requirements(),
-      classifiers=[]
+      classifiers=['Programming Language :: Python :: 3.5',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers']
       )

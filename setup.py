47062d3463a7e33d406dd1eff80d5e3344b16463
James T <jtara@tuta.io>
2017-12-21 14:42:41 -0800

    update setup.py version and fix possible error

--- setup.py
+++ setup.py
@@ -3,24 +3,23 @@ import re
 from os.path import join, dirname
 
 
+__file_path = dirname(__file__)
 version = '0.1.4'
 module_name = 'get_media_files'
 github_url = 'https://github.com/jtara1/{}'.format(module_name)
-with open(join(dirname(__file__), 'README.rst')) as f:
+with open(join(__file_path, 'README.rst')) as f:
     readme = f.read()
 
 
 def get_install_requirements():
     requirements = []
-    with open('requirements.txt', 'r') as f:
-        for line in f:
-            requirements.append(re.sub("\s", "", line))
-    print(requirements)
+    try:
+        with open(join(__file_path, 'requirements.txt'), 'r') as req_file:
+            for line in req_file:
+                requirements.append(re.sub("\s", "", line))
+    except (FileExistsError, FileNotFoundError):
+        pass
     return requirements
-
-
-if __name__ == "__main__":
-    pass
 
 
 setup(name=module_name,

44de88ebf99da436ce618e787d772a73f33e82e2
James T <jtara@tuta.io>
2017-12-21 16:11:35 -0800

    switch over to using PBR to compliment setuptools

--- setup.py
+++ setup.py
@@ -22,20 +22,6 @@ def get_install_requirements():
     return requirements
 
 
-setup(name=module_name,
-      packages=[module_name],
-      version=version,
-      description='Wrapper for pymediainfo that retrieves information such as'
-                  'media type, duration, resolution, etc. from media files',
-      long_description=readme,
-      author='James T',
-      author_email='jtara@tuta.io',
-      url=github_url,
-      download_url='{github_url}/archive/{version}.tar.gz'
-      .format(github_url=github_url, version=version),
-      keywords=['pymediainfo', 'wrapper', 'get media files', 'info'],
-      install_requires=get_install_requirements(),
-      classifiers=['Programming Language :: Python :: 3.5',
-                   'Environment :: Console',
-                   'Intended Audience :: Developers']
+setup(setup_requires=['pbr'],
+      pbr=True,
       )

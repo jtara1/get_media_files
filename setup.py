f74a8ce4d9603011447463c3cfd6c2e21695566e
James T <jtara@tuta.io>
2017-12-22 17:08:43 -0800

    change back to using std setup.py (no PBR)

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
-from distutils.core import setup
+from setuptools import setup
 import re
-from os.path import join, dirname
+from os.path import join, dirname, basename
 
 
 __file_path = dirname(__file__)
-version = '0.1.4'
-module_name = 'get_media_files'
-github_url = 'https://github.com/jtara1/{}'.format(module_name)
-with open(join(__file_path, 'README.rst')) as f:
-    readme = f.read()
+# get module name from parent folder name
+# assumes the parent folder (repository name) is the same as the module name
+module_name = basename(__file_path)
+
+# -------------- Update the following variables --------------- #
+version = '0.1.5'
+github_user = 'jtara1'
+author = 'James T'
+author_email = 'jtara@tuta.io'
+description = 'description_needed_from_dev'  # brief module description
+# ------------------------------------------------------------- #
+
+github_url = 'https://github.com/{}/{}'.format(github_user, module_name)
+download_url = '{github_url}/archive/{version}.tar.gz'\
+    .format(github_url=github_url, version=version)
+try:
+    with open(join(__file_path, 'README.rst')) as f:
+        readme = f.read()
+except (FileNotFoundError, FileExistsError):
+    try:
+        with open(join(__file_path, 'README.md')) as f:
+            readme = f.read()
+    except (FileExistsError, FileNotFoundError):
+        readme = description
 
 
 def get_install_requirements():
@@ -22,6 +41,24 @@ def get_install_requirements():
     return requirements
 
 
-setup(setup_requires=['pbr'],
-      pbr=True,
+setup(name=module_name,
+      packages=[module_name],
+      version=version,
+      description=description,
+      long_description=readme,
+      author=author,
+      author_email=author_email,
+      url=github_url,
+      download_url=download_url,
+      keywords=[],
+      install_requires=get_install_requirements(),
+      # list of strs https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[]
       )
+
+
+# setup(setup_requires=['pbr'],
+#       pbr=True,
+#       install_requires=get_install_requirements(),
+#       packages=find_packages(),
+#       )
